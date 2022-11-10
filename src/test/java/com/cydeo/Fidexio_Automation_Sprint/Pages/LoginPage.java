e88559ea3dddaf0a5622744271618507d5b55201
ertugbatar <ertugbatar@gmail.com>
2022-11-10 18:26:05 +0000

    LoginPage

--- src/test/java/com/cydeo/Fidexio_Automation_Sprint/Pages/LoginPage.java
+++ src/test/java/com/cydeo/Fidexio_Automation_Sprint/Pages/LoginPage.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class LoginPage {
 
     public LoginPage(){
-
         PageFactory.initElements(Driver.getDriver(), this);
+
 
     }
 
